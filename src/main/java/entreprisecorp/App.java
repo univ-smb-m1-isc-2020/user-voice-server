e6d4dabb3b5f073166562d4e05cf091d83c81419
TomLeCollegue <tomkubasik.pro@gmail.com>
2021-03-29 15:31:12 +0200

    dont work

--- src/main/java/entreprisecorp/App.java
+++ src/main/java/entreprisecorp/App.java
@@ -1,10 +1,15 @@
 package entreprisecorp;
 
 import entreprisecorp.database.*;
+import entreprisecorp.hibernate.UserRepository;
+import entreprisecorp.hibernate.UserRepositoryTest;
 import entreprisecorp.restservices.models.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class App {
